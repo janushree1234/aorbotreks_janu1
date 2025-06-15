a914e1c8fd826cd533250d976fdc95187a89eba8
janushree1234 <janushree.aorbotreks@gmail.com>
2025-06-16 01:24:49 +0530

    Fix: Force clean install of psycopg2-binary

--- build.sh
+++ build.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
-# build.sh
-
-# Exit immediately if a command exits with a non-zero status.
 set -o errexit
 
-# Install Python dependencies
-pip install -r requirements.txt
+pip install --upgrade pip
+pip install --no-cache-dir -r requirements.txt
 
-# Collect static files for deployment
 python manage.py collectstatic --no-input
-
-# Apply database migrations
 python manage.py migrate
