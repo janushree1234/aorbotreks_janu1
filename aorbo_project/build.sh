a09e22a76ce9a3f8089d614e63b2b9686bfd9b97
janushree1234 <janushree.aorbotreks@gmail.com>
2025-06-15 23:36:01 +0530

    Deploy ready

--- aorbo_project/build.sh
+++ aorbo_project/build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
-# build.sh
+# Exit script on any error
+set -o errexit
+
+# Install dependencies
 pip install -r requirements.txt
+
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Apply database migrations
 python manage.py migrate
