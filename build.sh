fc67ac0a680533bccf0b35ea250fe78b09c24d0b
janushree1234 <janushree.aorbotreks@gmail.com>
2025-06-16 01:30:30 +0530

    Fix psycopg2 error and ensure requirements.txt is in correct location

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install --upgrade pip
-pip install --no-cache-dir -r requirements.txt
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
